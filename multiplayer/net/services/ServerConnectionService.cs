73156cf08870d1451a57d4730bb122c84419964b
dutkow <149028694+dutkow@users.noreply.github.com>
2026-03-02 13:52:58 +0100

    Updates to folder structure

---
+++ multiplayer/net/services/ServerConnectionService.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public static class ServerConnectionService
+{
+    public static void HandleConnectionRequest(ENetPacketPeer peer, byte[] data)
+    {
+        var msg = new ConnectionRequest();
+        msg.ReadMessage(data);
+
+        NetworkSession.Instance.HandleConnectionRequest(peer, msg.PlayerName);
+
+        GD.Print($"Server received: {msg.MessageType}");
+    }
+
+    public static void HandleClientLoaded(ENetPacketPeer peer, byte[] data)
+    {
+        var msg = new ClientLoaded();
+        msg.ReadMessage(data);
+
+        //InitialMatchState.Send(peer, ) -> TODO: send this data
+
+        GD.Print($"Server received: {msg.MessageType}");
+    }
+}
