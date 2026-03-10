0bbf920e38d5db42425e7842c7d614568db89067
dutkow <149028694+dutkow@users.noreply.github.com>
2026-03-10 11:31:15 +0100

    adding chat message request class

--- network/messages/ChatMessageRequest.cs
+++ network/messages/ChatMessageRequest.cs
@@ -1,6 +1,49 @@
 using Godot;
 using System;
 
-public partial class ChatMessageRequest : Node
+public class ChatMessageRequest : Message
 {
+    public ChatChannel Channel;
+    public string Text;
+    public byte TargetPlayerID; // used only for private messages
+
+    protected override int BufferSize()
+    {
+        base.BufferSize();
+        Add(Channel);
+        Add(Text);
+        Add(TargetPlayerID);
+        return _dataSize;
+    }
+
+    public override byte[] WriteMessage()
+    {
+        base.WriteMessage();
+        Write(Channel);
+        Write(Text);
+        Write(TargetPlayerID);
+        return _data;
+    }
+
+    public override void ReadMessage(byte[] data)
+    {
+        base.ReadMessage(data);
+        Read(out Channel);
+        Read(out Text);
+        Read(out TargetPlayerID);
+    }
+
+    public static void Send(ENetPacketPeer client, ChatChannel channel, string text, byte targetPlayerID = 0)
+    {
+        var msg = new ChatMessage
+        {
+            MessageType = Msg.S2C_CHAT_MESSAGE,
+            ENetFlags = ENetPacketFlags.Reliable,
+            Channel = channel,
+            Text = text,
+            PlayerID = targetPlayerID,
+
+        };
+        NetworkSender.ToServer(msg);
+    }
 }
